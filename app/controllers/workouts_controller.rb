8a4eab3d2222d79796fcc0e4eb609fd0648d1af3
brendanbowidas <brendanbowidas@gmail.com>
2015-08-12 10:47:54 +0000

    added form, new, and show views

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,4 +2,44 @@ class WorkoutsController < ApplicationController
     def index 
         @workouts = Workout.all
     end
+    
+    def show 
+        @workout = Workout.find(params[:id])
+    end
+    
+    def new
+        @workout = Workout.new
+    end
+    
+    def create 
+        @workout = Workout.new(workout_params)
+        if @workout.save
+            redirect_to @workout
+            
+        else
+            render 'new'
+        end
+    end
+    
+    def edit 
+        
+    end
+    
+     def update 
+         
+     end
+     
+     def destroy 
+         
+     end
+     
+     private
+     def workout_params 
+     params.require(:workout).permit(:date, :workout, :comment, :length)
+         
+     end
+     
+     def find_workout 
+            
+     end
 end
