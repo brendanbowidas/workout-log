e2df26d1dfdef965dd47c9186b7fd6cd17a737f0
brendanbowidas <brendanbowidas@gmail.com>
2015-08-16 21:05:14 +0000

    image upload fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,9 @@ Rails.application.configure do
   :storage => :s3,
   :s3_protocol => 'https',
    :s3_credentials => {
-    :bucket => 'workout-log-img'
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
    :url =>':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_protocol => 'https',
-  :s3_credentials => {
-    :bucket => 'workout-log-img'
+   :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
    :url =>':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename',
